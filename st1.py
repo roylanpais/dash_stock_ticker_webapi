f8d12a884c77b0b315c7e8f67d57b6c3448cccff
Roylan Pais <roylanpais@gmail.com>
2023-04-02 21:36:34 +0530

    added button to select multiple symbols and also added the NASDAQ data

--- st1.py
+++ st1.py
@@ -4,36 +4,57 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output, State
 import pandas_datareader.data as web
+import pandas as pd
 from datetime import datetime
+
+nsdq=pd.read_csv("nasdaq_screener_1680450464862.csv")
+nsdq.set_index("Symbol", inplace=True)
+options=[]
+
+for tic in nsdq.index:
+    mydict={}
+    mydict["label"]=nsdq.loc[tic]["Name"] + " " + tic
+    mydict["value"]=tic
+    options.append(mydict)
+
 app=dash.Dash()
 os.environ["IEX_API_KEY"] ="pk_ff3a437410cd4fcfa1b9fdb44c4866f6"
+
 app.layout= html.Div([
-
-    html.H1("stock Ticker Dashboard"),
-    html.Div([html.H3('Enter a stock symbol:',style={"paddingRight":"30px"}),
-    dcc.Input(id="my_stock_picker", value="TSLA",style={"fontSize":24,"width":75})],style={"display":"inline-block","verticalAlign":"top"}),
-    html.Div([html.H3("Slect a start and end date:"),
-              dcc.DatePickerRange(id="my_date_picker",
-              min_date_allowed=datetime(2015,1,1),
-              max_date_allowed=datetime.today(),
-              start_date=datetime(2022,1,1),
-              end_date=datetime.today())
-              ],style={"display": "inline-block"}),
-    html.Div([html.Button(id="submit-button",
-                          n_clicks=0,
-                          children="Submit",
-                          style={"fontSize":24,"marginLeft":"30px"})
+    html.H1("Stock Ticker Dashboard"),
     
+    html.Div([
+        html.H3('Select stock symbols:', style={"paddingRight":"30px"}),
     
+        dcc.Dropdown(
+            id="my_stock_picker", 
+            options=options, 
+            value=["TSLA"], multi=True
+            )],
+            style={"display":"inline-block","verticalAlign":"top","width": "30%"}),
+    
+    html.Div([html.H3("Select a start and end date:"),
+            dcc.DatePickerRange(
+                id="my_date_picker",
+                min_date_allowed=datetime(2015,1,1),
+                max_date_allowed=datetime.today(),
+                start_date=datetime(2022,1,1),
+                end_date=datetime.today())
+                ],style={"display": "inline-block"}),
 
-    ],style={"display":"inline"}),       
+    html.Div([html.Button(
+            id="submit-button",
+            n_clicks=0,
+            children="Submit",
+            style={"fontSize":24,"marginLeft":"30px"})
+            ],style={"display":"inline"}),       
 
     dcc.Graph(id="my_graph", 
               figure={"data":
                       [{"x":[1,2],"y":[3,1]}],
-                      "layout":{"title":"default title"}                      
-    })
-])
+                    #   "layout":{"title":"default title"}                      
+        })
+    ])
 
 @app.callback(Output("my_graph","figure"),
               [Input("submit-button","n_clicks")],
@@ -42,11 +63,15 @@ app.layout= html.Div([
 def update_graph(n_clicks, stock_ticker,start_date,end_date):
     start=datetime.strptime(start_date[:10],"%Y-%m-%d")
     end=datetime.strptime(end_date[:10],"%Y-%m-%d")
-    df=web.DataReader(stock_ticker,"iex",start,end)
-    fig={"data":
-            [{"x":df.index,"y":df["close"]}],
-            "layout":{"title":stock_ticker}                   
-    }
+
+    traces=[]
+
+    for tic in stock_ticker:
+        
+        df=web.DataReader(tic,"iex",start,end)
+        traces.append({"x":df.index,"y":df["close"], "name":tic})
+    fig={"data": traces,
+        "layout":{"title":", ".join(stock_ticker) + " closing prices"}}
     return fig
 
 
